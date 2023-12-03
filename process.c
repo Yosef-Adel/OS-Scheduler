9dd1b6162bebda66bb702baba06cc35e73ded075
Yosef <yosef.saaid02@eng-st.cu.edu.eg>
2023-12-03 23:43:20 +0200

    adding process functunality

--- process.c
+++ process.c
@@ -1,20 +1,56 @@
 #include "headers.h"
 
 /* Modify this file as needed*/
-int remainingtime;
+// Variables needed for the time info
 
-int main(int agrc, char * argv[])
-{
+int remaining_time,run_time,wait_time = 0,start_time,sleep_time = 0;
+
+// Overwriting the SIGTSTP
+void stopProcess(int signum){
+    sleep_time = getClk();
+    raise(SIGSTOP);
+    // assuring the handler is set
+    signal(SIGTSTP, stopProcess);
+}
+
+// Overwriting the SIGCONT
+void resumeProcess(int signum){
+    wait_time += getClk() - sleep_time;
+    // assuring the handler is set
+    signal(SIGCONT,resumeProcess);
+}
+
+int main(int agrc, char * argv[]){
+    // Setting handlers
+    signal(SIGTSTP,stopProcess);
+    signal(SIGCONT,resumeProcess);
+
     initClk();
+
     
     //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
-    while (remainingtime > 0)
-    {
-        // remainingtime = ??;
+    start_time = getClk();
+    run_time = atoi(argv[1]);
+
+    printf("Runtime %d\n",run_time);
+    fflush(stdout);
+
+    remaining_time = run_time;
+
+    printf("remaining %d\n",remaining_time);
+    fflush(stdout);
+
+    while (remaining_time > 0){
+        remaining_time = run_time - (getClk() - start_time - wait_time);
     }
+
     
+    int end = getClk();
+    
+
     destroyClk(false);
-    
+    printf("Exit\n");
+    fflush(stdout);
+    exit(0);
     return 0;
 }
