ddd67ef4f611ca379ccb9c56599218840fda78c4
Yosef <yosef.saaid02@eng-st.cu.edu.eg>
2023-12-04 12:42:11 +0200

    implent HPF algo

---
+++ HPF.h
@@ -0,0 +1,63 @@
+// TODO implement HPF
+
+#pragma once
+#include "headers.h"
+#include "priority_queue.h"
+
+bool HPFaddProcess(void *type, process* p);
+bool HPFpreempt(void *type);
+process* HPFgetNextProcess(void *type);
+bool HPFremoveProcess(void *type, process* p);
+bool HPFfree(void *type);
+
+
+bool HPFInitialize(scheduling_algo* current_algorithm) {
+    if(!current_algorithm) return 0;
+
+    priority_queue* HPFqueue = (priority_queue*)malloc(sizeof(priority_queue));
+    createQueue(HPFqueue, 50);
+
+    *current_algorithm = (scheduling_algo){
+        HPFqueue,
+        HPFaddProcess,
+        HPFpreempt,
+        HPFgetNextProcess,
+        HPFremoveProcess,
+        HPFfree,
+        
+    };
+    return 1;
+}
+
+
+
+bool HPFaddProcess(void *type, process* p){
+    priority_queue *pq = (priority_queue*)type;
+    bool success = enqueue(pq, p);
+    return success;
+}
+
+process* HPFgetNextProcess(void *type){
+    priority_queue *pq = (priority_queue*)type;
+    process *p = getpeek(pq);
+    return p;
+}
+
+bool HPFpreempt(void *type){ // take care of the process that is currently running 
+    if(curentProcess){
+        return false;
+    }
+    return true;
+}
+
+bool HPFremoveProcess(void *type, process* p){
+    priority_queue *pq = (priority_queue*)type;
+    bool success = removeProcess(pq, p);
+    return success;
+}
+
+bool HPFfree(void *type){
+    priority_queue *pq = (priority_queue*)type;
+    free(pq);
+    return true;
+}
