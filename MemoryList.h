0f8051fbba853a22558769370d5315f6ad52d64e
Ziad <75042260+ziadmali59@users.noreply.github.com>
2023-12-25 11:53:37 +0200

    Add files via upload
    implemented Linkedlist fucntion for holes (0-->10 :1-->1024)

--- MemoryList.h
+++ MemoryList.h
@@ -84,3 +84,97 @@ void deleteHole(memList *mlist, int addresss)
   sortHoles(mlist);
 }
 
+
+/**
+ * This function returns the minimum address in the memory list.
+ * It iterates through the list and compares each address with the current minimum.
+ * If a smaller address is found, it updates the minimum.
+ * If the list is empty, it returns -1.
+ * 
+ * @param mlist Pointer to the memory list.
+ * @return The minimum address in the memory list, or -1 if the list is empty.
+ */
+int getminadderess(memList *mlist) 
+{
+
+  int min = INT_MAX;
+
+  struct node *ptr = mlist->head;
+
+  while (ptr != NULL) {
+
+    if (ptr->addresss < min)
+      min = ptr->addresss;
+    ptr = ptr->next;
+  }
+
+  if (min != INT_MAX)
+    return min;
+  else
+    return -1;
+}
+
+int ExtractFirstAddress(memList *mList) 
+{
+  int min = getminadderess(mList);
+  deleteHole(mList, min);
+  return min;
+}
+///////////////////////////////////check output ////////////////////////////////
+void display(memList *mlist) 
+{
+
+  struct node *current = mlist->head;
+  if (current == NULL) {
+    printf("list is empty");
+    return;
+  }
+  while (current != NULL) {
+    printf("%d", current->addresss);
+    current = current->next;
+  }
+  printf("\n");
+}
+/////////////////////////////////////////////////////////////////////////////////
+
+void sortHoles(memList *mlist) 
+{
+  int a;
+
+  struct node *h = mlist->head;
+
+  struct node *temp1;
+  struct node *temp2;
+
+  for (temp1 = h; temp2 != NULL; temp1 = temp1->next) {
+    for (temp2 = temp1->next; temp2 != NULL; temp2 = temp2->next) {
+      if (temp2->addresss < temp1->addresss) {
+        a = temp1->addresss;
+        temp1->addresss = temp2->addresss;
+        temp2->addresss = a;
+
+      }
+    }
+  }
+}
+
+int compactList(memList *mlist) 
+{
+  struct node *temp1;
+  struct node *temp2;
+
+  for (temp1 = mlist->head; temp1 != NULL; temp1 = temp1->next) {
+    for (temp2 = temp1->next; temp2 != NULL; temp2 = temp2->next) {
+      if (temp1->addresss % (int)(2 * pow(2, mlist->index)) == 0 &&
+          temp1->addresss + pow(2, mlist->index) == temp2->addresss) {
+
+        // compact
+        int addr = temp1->addresss;
+        deleteHole(mlist, temp1->addresss);
+        deleteHole(mlist, temp2->addresss);
+        return addr;
+      }
+    }
+  }
+  return -1;
+}
